85520a692ceb6174e6cb4f772f65c3fc9f5549a5
Melloman <lovelyboyy@mail.ru>
2022-11-01 23:55:08 +0300

    i

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,6 @@
 public class Car implements AutoCloseable {
     @Override
-    public void close() throws Exception {
+    public void close() {
         System.out.println("Машина закрывается");
     } void drive(){
         System.out.println("Машина журуп баратат");
