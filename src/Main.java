83e5b9175bccefa786dabf1118ec7569118896b2
Melloman <lovelyboyy@mail.ru>
2022-11-01 23:59:35 +0300

    i

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         try (Car car = new Car()){
             car.drive();
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
                 e.printStackTrace();
         }
     }
